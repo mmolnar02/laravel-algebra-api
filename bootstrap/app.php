3e771097904bf3a1abfac3ab1b418aa73a992265
Tomislav Kešcec <tkescec@algebra.hr>
2024-07-09 11:37:34 +0200

    Predavanje01

---
+++ bootstrap/app.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        api: __DIR__.'/../routes/api.php',
+        apiPrefix: 'api/v1',
+        //health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->renderable(
+            function (Throwable $e) {
+                return match (get_class($e)) {
+                    NotFoundHttpException::class => response()->json(['message' => 'Not Found'], 404),
+                    default => response()->json(['message' => 'Bad Request'], 400),
+                };
+            }
+        );
+    })->create();
